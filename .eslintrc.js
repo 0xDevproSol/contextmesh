36ffa17de4f6b1a49a946dad36488fc8ce04e3a4
0xDevproSol <contextmesh@gmail.com>
2025-05-01 01:15:15 +0200

    chore(lint): add ESLint configuration for TypeScript and Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  },
+  env: {
+    node: true,
+    es2020: true,
+    jest: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+  ],
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'no-console': 'warn',
+    'prettier/prettier': 'error',
+    '@typescript-eslint/no-explicit-any': 'off'
+  }
+};
